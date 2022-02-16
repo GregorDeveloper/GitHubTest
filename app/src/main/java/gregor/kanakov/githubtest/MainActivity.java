ba6f890505f592417562444712544b03a4dfa5b0
gregor <sirgregordevelope@gmail.com>
2022-02-16 12:57:31 +0300

    test 1

--- app/src/main/java/gregor/kanakov/githubtest/MainActivity.java
+++ app/src/main/java/gregor/kanakov/githubtest/MainActivity.java
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    String test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
         //test repository
         // +++
 
+    }
+
+    private void init(){
+        test = "test";
     }
 }
